0ab7845d7bfa84e3d319586a589b6c8752e9e391
kr <hoylfree@gmail.com>
2022-03-21 23:30:00 +0900

    1,2,3더하기

--- 220321/[boj] 1,2,3더하기.py
+++ 220321/[boj] 1,2,3더하기.py
@@ -1,15 +1,34 @@
-T = int(input())
-n = [0] * 11
-n[1] = 1
-n[2] = 2
-n[3] = 4
+# T = int(input())
+# n = [0] * 11
+# n[1] = 1
+# n[2] = 2
+# n[3] = 4
 
-for i in range(4, 11):
-    n[i] = n[i-3] + n[i-2] + n[i-1]
+# for i in range(4, 11):
+#     n[i] = n[i-3] + n[i-2] + n[i-1]
+
+# for _ in range(T):
+#     m = int(input())
+#     print(n[m])
+
+
+T = int(input())
+
+
+def sol(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 4
+    else:
+        return sol(n-3) + sol(n-2) + sol(n-1)
+
 
 for _ in range(T):
-    m = int(input())
-    print(n[m])
+    n = int(input())
+    print(sol(n))
 
 
 def backtracking(sum, n):
