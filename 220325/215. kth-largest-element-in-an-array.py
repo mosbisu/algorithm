b9d61a23b5889f71ceb72e016b03cf40e5fd61d4
kr <hoylfree@gmail.com>
2022-03-25 17:37:01 +0900

    힙

---
+++ 220325/215. kth-largest-element-in-an-array.py
@@ -0,0 +1,65 @@
+from typing import List
+
+
+class BinaryMaxHeap:
+    def __init__(self):
+        self.items = [None]
+
+    def __len__(self):
+        return len(self.items) - 1
+
+    def _percolate_up(self):
+        cur = len(self)
+        parent = cur // 2
+
+        while parent > 0:
+            if self.items[cur] > self.items[parent]:
+                self.items[cur], self.items[parent] = self.items[parent], self.items[cur]
+
+            cur = parent
+            parent = cur // 2
+
+    def _percolate_down(self, cur):
+        biggest = cur
+        left = 2 * cur
+        right = 2 * cur + 1
+
+        if left <= len(self) and self.items[left] > self.items[biggest]:
+            biggest = left
+
+        if right <= len(self) and self.items[right] > self.items[biggest]:
+            biggest = right
+
+        if biggest != cur:
+            self.items[cur], self.items[biggest] = self.items[biggest], self.items[cur]
+            self._percolate_down(biggest)
+
+    def insert(self, k):
+        self.items.append(k)
+        self._percolate_up()
+
+    def extract(self, k):
+        root = None
+
+        while k > 0:
+            root = self.items[1]
+            self.items[1] = self.items[-1]
+            self.items.pop()
+            self._percolate_down(1)
+            k -= 1
+
+        return root
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = BinaryMaxHeap()
+        for num in nums:
+            heap.insert(num)
+
+        return heap.extract(k)
+
+
+class Solution2:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        return sorted(nums, reverse=True)[k-1]
---
+++ 220325/[boj] 최대 힙.py
@@ -0,0 +1,68 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+
+class BinaryMaxHeap:
+    def __init__(self):
+        self.items = [None]
+
+    def __len__(self):
+        return len(self.items) - 1
+
+    def _percolate_up(self):
+        cur = len(self)
+        parent = cur // 2
+
+        while parent > 0:
+            if self.items[cur] > self.items[parent]:
+                self.items[cur], self.items[parent] = self.items[parent], self.items[cur]
+
+            cur = parent
+            parent = cur // 2
+
+    def _percolate_down(self, cur):
+        biggest = cur
+        left = 2 * cur
+        right = 2 * cur + 1
+
+        if left <= len(self) and self.items[left] > self.items[biggest]:
+            biggest = left
+
+        if right <= len(self) and self.items[right] > self.items[biggest]:
+            biggest = right
+
+        if biggest != cur:
+            self.items[cur], self.items[biggest] = self.items[biggest], self.items[cur]
+            self._percolate_down(biggest)
+
+    def insert(self, k):
+        self.items.append(k)
+        self._percolate_up()
+
+    def extract(self):
+        if len(self) < 1:
+            return None
+
+        root = self.items[1]
+        self.items[1] = self.items[-1]
+        self.items.pop()
+        self._percolate_down(1)
+
+        return root
+
+
+N = int(input())
+arr = [int(input()) for _ in range(N)]
+heap = BinaryMaxHeap()
+
+for x in arr:
+    if x == 0:
+        if len(heap) == 0:
+            print(0)
+            continue
+        else:
+            print(heap.extract())
+            continue
+
+    heap.insert(x)
