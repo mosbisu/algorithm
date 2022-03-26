69ea16ac31e0920e226fcba0035175ce3cea828c
kr <hoylfree@gmail.com>
2022-03-26 14:40:46 +0900

    정렬

---
+++ 220326/147. insertion-sort-list.py
@@ -0,0 +1,39 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = parent = ListNode(None)
+
+        while head:
+            while cur.next and cur.next.val < head.val:
+                cur = cur.next
+
+            cur.next, head.next, head = head, cur.next, head.next
+            cur = parent
+
+        return cur.next
+
+
+class Solution2:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # 초깃값 변경
+        cur = parent = ListNode(0)
+
+        while head:
+            while cur.next and cur.next.val < head.val:
+                cur = cur.next
+
+            cur.next, head.next, head = head, cur.next, head.next
+
+            # 필요한 경우에만 cur 포인터가 되돌아가도록 처리
+            if head and cur.val > head.val:
+                cur = parent
+
+        return parent.next
