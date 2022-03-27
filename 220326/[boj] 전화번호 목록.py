0a744158710ae1626e7d77507ef24d23aec8e205
kr <hoylfree@gmail.com>
2022-03-27 21:29:30 +0900

    전화번호목록

---
+++ 220326/[boj] 전화번호 목록.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    phone = [input().rstrip() for _ in range(n)]
+    phone.sort()
+
+    for i in range(n-1):
+        length = len(phone[i])
+        if phone[i] == phone[i+1][:length]:
+            print('NO')
+            break
+    else:
+        print('YES')
